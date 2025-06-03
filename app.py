c024c8f7f3de14f4b28218652cf69bc52ff53aec
Prachi Singh <111281636+Msblack2904@users.noreply.github.com>
2025-06-03 19:00:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,32 @@
 import streamlit as st
 import pickle
 import string
-import nltk
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
+import re
+
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
 ps = PorterStemmer()
 
-# Ensure necessary NLTK data is downloaded
-def download_nltk_resources():
-    try:
-        nltk.data.find('tokenizers/punkt')
-    except LookupError:
-        nltk.download('punkt')
-    try:
-        nltk.data.find('corpora/stopwords')
-    except LookupError:
-        nltk.download('stopwords')
-
-download_nltk_resources()
-
 # Load model and vectorizer
-model = pickle.load(open('model.pkl','rb'))
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+
+# Lightweight tokenizer instead of word_tokenize
+def simple_tokenize(text):
+    return re.findall(r'\b\w+\b', text.lower())
 
 # Text preprocessing
 def transform_text(text):
-    text = text.lower()
-    text = word_tokenize(text)
-    
+    text = simple_tokenize(text)
     y = [i for i in text if i.isalnum()]
-    
     y = [i for i in y if i not in stopwords.words('english') and i not in string.punctuation]
-    
     y = [ps.stem(i) for i in y]
-    
     return " ".join(y)
 
 # Streamlit UI
 def main():
-    st.title("Email/SMS Spam Classifier")
+    st.title("📩 Email/SMS Spam Classifier")
     input_sms = st.text_area('Enter the message')
     
     if st.button('Predict'):
@@ -59,4 +41,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
